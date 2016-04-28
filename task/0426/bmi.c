80576b2432c21419e27a501f882eeb9dd5e152a6
1119ab <t.syunsuke111965@gmail.com>
2016-04-28 21:36:25 +0900

    add task No1

---
+++ task/0426/bmi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+float main(){
+
+	printf ("How weight are you?\n");
+	float num1,num2;
+	scanf ("%f",&num1);
+	printf("How height are you?\n");
+	scanf ("%f",&num2);
+	printf("Your BMI is %f\n.",num1/num2/num2);
+}
